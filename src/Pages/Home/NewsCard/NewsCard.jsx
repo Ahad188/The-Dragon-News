c2b98862cdba44f557d07ef17330ec82d73baa71
AhaD <abuahad35188@gmail.com>
2023-04-28 23:22:26 +0600

    rating add

--- src/Pages/Home/NewsCard/NewsCard.jsx
+++ src/Pages/Home/NewsCard/NewsCard.jsx
@@ -1,12 +1,27 @@
+import moment from "moment";
 import React from "react";
-import { Card } from "react-bootstrap";
+
+import { Card, Image } from "react-bootstrap";
+import { FaEye, FaRegBookmark, FaRegStar, FaShareAlt, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 import { Link } from "react-router-dom";
 
 const NewsCard = ({ news }) => {
   const { _id, title, details, image_url, author, rating, total_view } = news;
   return (
-    <Card className="text-center">
-      <Card.Header>Featured</Card.Header>
+    <Card className="mb-4">
+      <Card.Header className="d-flex align-items-center">
+        <Image style={{ height: "40px" }} src={author?.img} roundedCircle />
+        <div className="ps-2 flex-grow-1">
+          <p className="mb-0">{author?.name}</p>
+          <p>
+            <small> {moment(author?.published_date).format("yyyy-MM-D")}</small>
+          </p>
+        </div>
+        <div>
+          <FaRegBookmark></FaRegBookmark> <FaShareAlt></FaShareAlt>
+        </div>
+      </Card.Header>
       <Card.Body>
         <Card.Title>{title}</Card.Title>
         <Card.Img variant="top" src={image_url} />
@@ -20,7 +35,21 @@ const NewsCard = ({ news }) => {
           )}
         </Card.Text>
       </Card.Body>
-      <Card.Footer className="text-muted">2 days ago</Card.Footer>
+      <Card.Footer className="text-muted d-flex">
+        <div className="flex-grow-1 d-flex align-items-center">
+          <Rating
+            placeholderRating={rating.number}
+            readonly
+            emptySymbol={<FaRegStar></FaRegStar>}
+            placeholderSymbol={<FaStar className="text-warning"></FaStar>}
+            fullSymbol={<FaStar></FaStar>}
+          ></Rating>
+          <span className="ms-2"> {rating?.number}</span>
+        </div>
+        <div>
+          <FaEye></FaEye> {total_view}
+        </div>
+      </Card.Footer>
     </Card>
   );
 };
